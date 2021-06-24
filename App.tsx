5c589f7bd586ec732b121fb7a381046fe037184b
Tiago Garcia <tiagovtg@gmail.com>
2021-06-23 21:11:28 -0300

    gameplay aula 2,3,4

--- App.tsx
+++ App.tsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React from 'react';
+import { StatusBar, LogBox } from 'react-native';
+import { Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
-import { useFonts } from "expo-font";
-import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
-import { Rajdhani_500Medium, Rajdhani_700Bold } from "@expo-google-fonts/rajdhani";
+LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine.']);
 
-import { AppLoading } from "expo-app-loading";
-import { SignIn } from "./src/screens/SignIn";
+import { AuthProvider } from './src/hooks/auth';
 
-export default function App() {
-  useFonts( {
+import { Routes } from './src/routes';
+import { Background } from './src/components/Background';
+
+export default function App(){
+  const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium,
     Rajdhani_500Medium,
-    Rajdhani_700Bold,
-  } );
+    Rajdhani_700Bold
+  });
 
-  return <SignIn />;
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+
+  return(
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </Background>
+  );
 }
